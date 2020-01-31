2ce1fc6a89a842538ce3321268547b38c39af9c7
ciniki <ciniki@protonmail.com>
2020-01-31 17:31:12 +0800

    更新私聊指令

--- app.js
+++ app.js
@@ -14,6 +14,29 @@ var monitorGroup = 123456;
 
 var mode = 2;
 
+function sendMessage(account, message)
+{
+    var postData = {
+        "account": account,
+        "msg": message
+    };
+
+    var req = http.request(
+        {
+            host: '127.0.0.1',
+            port: '36524',
+            method: 'POST',
+            path: '/api/v1/Cqp/CQ_sendPrivateMsg',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+
+    req.write(JSON.stringify(postData));
+    req.end();
+}
+
 //Perform a ban to a group member with mahua api
 function sendRequest(group, account, time)
 {
@@ -52,9 +75,12 @@ function execute(mode) {
         
         //Random mode: Ban a repeater randomly
         case 2: {
+            for (var i = 0; i<repeaterBuffer.length; i++) {
+                console.log(repeaterBuffer[i]);
+            }
             var index = Math.floor(Math.random()*(repeaterBuffer.length));
             sendRequest(monitorGroup, repeaterBuffer[index], executeTime);
-            console.log(index);
+            console.log("\n"+repeaterBuffer[index]);
 
             break;
         }
@@ -96,6 +122,57 @@ app.post('/api/ReceiveMahuaOutput', jsonParser, function (req, res) {
         else {
             repeatTime++;
             repeaterBuffer.push(data.FromQQ);
+        }
+    }
+
+    if(data.SubType == 11) {
+        console.log("Settings\n");
+        switch (data.Msg) {
+            case '普通模式' : {
+                mode = 1;
+                sendMessage(data.FromQQ, '已切换至普通模式');
+                break;
+            }
+            case '轮盘赌' : {
+                mode = 2;
+                sendMessage(data.FromQQ, '已切换至轮盘赌模式');
+                break;
+            }
+            case '强力模式' : {
+                mode = 3;
+                sendMessage(data.FromQQ, '已切换至强力模式');
+                break;
+            }
+            case '运行状态' : {
+                sendMessage(data.FromQQ, 'mode = '+mode+'\n'+
+                                         '检测次数 '+executeBorder+'\n'+
+                                         '禁言时间 '+executeTime+' 秒');
+                //sendMessage(data.FromQQ, '检测次数 '+executeBorder);
+                //sendMessage(data.FromQQ, '禁言时间 '+executeTime+' 秒');
+                break;
+            }
+            default : {
+                if(data.Msg>60) {
+                    executeTime = data.Msg;
+                    sendMessage(data.FromQQ, '禁言时间已调整为 '+executeTime+' 秒');
+                }
+                else if(data.Msg<10){
+                    executeBorder = data.Msg;
+                    sendMessage(data.FromQQ, '检测次数已调整为 '+executeBorder+' 次');
+                }
+                else {
+                    sendMessage(data.FromQQ, '不是合法的指令！\n'+
+                                             '输入 普通模式/轮盘赌/强力模式 调整工作模式\n'+
+                                             '输入小于10的整数调整检测次数\n'+
+                                             '输入大于60的整数调整禁言时间\n'+
+                                             '输入 运行状态 查看参数设置请况');
+                    //sendMessage(data.FromQQ, '输入 普通模式/轮盘赌/强力模式 调整工作模式');
+                    //sendMessage(data.FromQQ, '输入小于10的整数调整检测次数');
+                    //sendMessage(data.FromQQ, '输入大于60的整数调整禁言时间');
+                    //sendMessage(data.FromQQ, '输入 运行状态 查看参数设置请况');
+                }
+                break;
+            }
         }
     }
 
